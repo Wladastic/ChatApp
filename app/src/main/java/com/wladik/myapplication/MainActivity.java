d924d56b637c4a2b9b372bc2a6517b526b4a70b9
Wladislav Cugunov <wlad.c@me.com>
2016-10-01 22:24:59 +0200

    messing around

--- app/src/main/java/com/wladik/myapplication/MainActivity.java
+++ app/src/main/java/com/wladik/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.wladik.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button send_button = (Button) findViewById(R.id.send_button);
+        Button call_button = (Button) findViewById(R.id.call_button);
+        TextView text_view = (TextView) findViewById(R.id.text_view);
+
+
+        send_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                /**
+                 * TODO: add a function that adds the Text to the textfield.
+                 */
+                sendText();
+            }
+
+    });
     }
+    private void sendText() {
+
+        // this does nothing, just a test:
+        findViewById(R.id.message_text).getTag();
+        findViewById(R.id.text_view);
+
+    }
+
+
+
+
+
 }

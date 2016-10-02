664cb0cd56e3522a83ced2673d0868e4cd5acf0e
Wladislav Cugunov <wlad.c@me.com>
2016-10-02 22:32:53 +0200

    Functioning Send_text

--- app/src/main/java/com/wladik/myapplication/MainActivity.java
+++ app/src/main/java/com/wladik/myapplication/MainActivity.java
@@ -5,37 +5,73 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String username = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button send_button = (Button) findViewById(R.id.send_button);
+        Button send_button = (Button) findViewById(R.id.send_button);
         Button call_button = (Button) findViewById(R.id.call_button);
-        final TextView text_view = (TextView) findViewById(R.id.text_view);
+        final Button login_button = (Button) findViewById(R.id.login_button);
 
-        text_view.setText("");
+        final TextView login_name = (TextView) findViewById(R.id.login_name);
+        final TextView text_view = (TextView) findViewById(R.id.text_view);
+        final EditText message_text = (EditText) findViewById(R.id.message_text);
+
+/**
+ * TODO: Add username_id that calculates out of the user's IP adress or IMEI
+ *
+ */
+
         send_button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 /**
                  * TODO: add a function that adds the Text to the textfield.
                  */
-                sendText(text_view);
+                if(username.length() > 0) {
+                    if (String.valueOf(message_text.getText()).length() > 0){
+                        sendText(text_view, message_text, username);
+                    }
+                }
+
+            }
+        });
+
+        login_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                /**
+                 * TODO: add a function that adds the Text to the textfield.
+                 */
+                if(username.equals("")) {
+                    username = "wlad";
+                    login(username, login_name);
+                    login_button.setText("Ausloggen");
+                } else {
+                    username = "";
+                    login_name.setText("");
+                }
             }
 
-    });
+        });
     }
-    private void sendText(TextView text_view) {
+    private void login(String username, TextView login_name) {
+        login_name.setText(username);
+    }
+    private void sendText(TextView text_view, EditText message_text, String username) {
 
-        String text_to_send = "new text";
+        String text_to_send = String.valueOf(message_text.getText());
         // this does nothing, just a test:
-        text_view.setText(text_to_send);
-
+        String old_text_view = String.valueOf(text_view.getText());
+        text_view.setText(( old_text_view+ "\n" + username +":: " + text_to_send));
+        message_text.setText("");
     }
 
 
